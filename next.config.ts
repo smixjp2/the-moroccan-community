50ebc42a26f155b1a1d07cbb5fde146a346e5b23
Mohammed Serrou <serroumohammed7@gmail.com>
2025-12-28 19:04:08 +0000

    OK

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https://picsum.photos',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
