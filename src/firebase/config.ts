955abe423dcfd31949b532da46842d6be66f44b2
Mohammed Serrou <serroumohammed7@gmail.com>
2025-12-31 12:52:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-853998877-82b19",
+  "appId": "1:847886524300:web:9fd36585d77c6f38db80a9",
+  "apiKey": "AIzaSyAqny-_SUPGjKBENEwgdeIbeCilB-_UzpI",
+  "authDomain": "studio-853998877-82b19.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "847886524300"
+};
