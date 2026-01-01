5186f29fa8e588773ed40e97e8d3e8f23abfb4f8
Mohammed Serrou <serroumohammed7@gmail.com>
2026-01-01 19:13:46 +0000

    je veux voir mon siteweb

--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-// This is a temporary file to allow the middleware to redirect to the default locale.
-// It can be removed if a root page is ever created.
-export default function RootPage() {
-  return null;
-}
