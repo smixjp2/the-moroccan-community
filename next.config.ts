f670da0ba1e93924831860c8b7d1324ba0a88c4e
Mohammed Serrou <serroumohammed7@gmail.com>
2026-01-01 09:57:26 +0000

    UTILISE CETTE IMAGE https://sl.bing.net/gUqf9GXl3xA

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'sl.bing.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   devServer: {
