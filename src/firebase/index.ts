f549a5387c9a235c0ce16a4c537d178e090ff983
Mohammed Serrou <serroumohammed7@gmail.com>
2026-01-03 12:32:56 +0000

    DACCORD

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,33 +5,40 @@ import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth, connectAuthEmulator } from 'firebase/auth';
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
 
+let firebaseApp: FirebaseApp;
+
+// Initialize Firebase
+if (getApps().length === 0) {
+  firebaseApp = initializeApp(firebaseConfig);
+} else {
+  firebaseApp = getApp();
+}
+
+const auth = getAuth(firebaseApp);
+const firestore = getFirestore(firebaseApp);
+
+if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
+  // Check if emulators are already running to avoid errors on hot reloads
+  // @ts-ignore
+  if (!auth.emulatorConfig) {
+    connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
+  }
+  // @ts-ignore
+  if (!firestore.emulator) {
+     connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
+  }
+}
+
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
-    const firebaseApp = initializeApp(firebaseConfig);
-    return getSdks(firebaseApp);
-  }
-
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+  return {
+    firebaseApp,
+    auth,
+    firestore
+  };
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
-  const auth = getAuth(firebaseApp);
-  const firestore = getFirestore(firebaseApp);
-
-  if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
-    // Check if emulators are already running to avoid errors on hot reloads
-    // @ts-ignore
-    if (!auth.emulatorConfig) {
-      connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
-    }
-    // @ts-ignore
-    if (!firestore.emulator) {
-       connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
-    }
-  }
-
   return {
     firebaseApp,
     auth,
