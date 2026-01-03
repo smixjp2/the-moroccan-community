2719339a8e5af943e0b5d85d0d1cb27a52cdb169
Mohammed Serrou <serroumohammed7@gmail.com>
2026-01-03 19:33:53 +0000

    le probeleme reste toujours

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,7 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'sl.bing.net',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  // Forcing a cache clear again to resolve a stubborn build error.
+  experimental: {},
 };
 
 export default nextConfig;
