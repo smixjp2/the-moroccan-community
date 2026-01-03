ef351bc9ffc04c5ef814cb884a6073f01925497f
Mohammed Serrou <serroumohammed7@gmail.com>
2026-01-03 20:56:51 +0000

    21:55:01.834 Running build in Washington, D.C., USA (East) – iad1 21:55:

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        iconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        iconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        iconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        iconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
